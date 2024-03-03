d491fa7778c3c2eceb931f5caa3bd41b35cab6ec
winniewu550 <146806570+winniewu550@users.noreply.github.com>
2024-03-03 13:57:15 -0500

    Update fat-tree.py

--- fat-tree.py
+++ fat-tree.py
@@ -9,26 +9,18 @@ topology enables one to pass in '--topo=mytopo' from the command line.
 """
 
 from mininet.topo import Topo
-
+N=4
 class MyTopo( Topo ):
-    "fat-tree topology."
-
-    def build( self ):
-        "Create custom topo."
-
-        # Add hosts and switches
-        coreSwitch1 = self.addSwitch('c1')
-        coreSwitch2 = self.addSwitch('c2')
-
-        for i in range(4):
-            edgeSwitch = self.addSwitch(f'e{i+1}')
-            self.addLink(edgeSwitch, coreSwitch1)
-            self.addLink(edgeSwitch, coreSwitch2)
-
-            # Connect 2 servers to each edge switch
-            for j in range(2):
-                server = self.addHost(f'h{i*2+j+1}')
-                self.addLink(server, edgeSwitch)
-
-
+  def build(self, n=N):
+    if n % 2 != 0:
+      raise ValueError("N must be an even number.")
+    coreSwitch1 = self.addSwitch('c1')
+    coreSwitch2 = self.addSwitch('c2')
+    for i in range(n):
+      edgeSwitch = self.addSwitch('e{}'.format(i+1))
+      self.addLink(edgeSwitch, coreSwitch1)
+      self.addLink(edgeSwitch, coreSwitch2)
+      for j in range(n/2):
+        server = self.addHost('h{}'.format(i*2+j+1))
+        self.addLink(server, edgeSwitch)
 topos = { 'mytopo': ( lambda: MyTopo() ) }
