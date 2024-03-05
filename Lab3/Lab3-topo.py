6e6ade4a075e1a65a75be4084b3f1351853165cb
biubiubiupiu21 <197yjy@gmail.com>
2024-03-05 14:27:23 -0500

    Create Lab3 and lab3 topo

---
+++ Lab3/Lab3-topo.py
@@ -0,0 +1,35 @@
+
+from mininet.topo import Topo
+
+class MyTopo( Topo ):
+    "Simple topology example."
+
+    def build( self ):
+        "Create custom topo."
+
+        # Add hosts and switches
+        Host1 = self.addHost( 'h1' )
+        Host2 = self.addHost( 'h2' )
+        Host3 = self.addHost( 'h3' )
+        Host4 = self.addHost( 'h4' )
+
+        Switch1 = self.addSwitch( 's1' )
+        Switch2 = self.addSwitch( 's2' )
+        Switch3 = self.addSwitch( 's3' )
+        Switch4 = self.addSwitch( 's4' )
+
+        # Add links
+        self.addLink( Host1, Switch1 )
+        self.addLink( Switch1, Switch2 )
+
+        self.addLink( Host2, Switch2 )
+        self.addLink( Switch2, Switch3 )
+
+        self.addLink( Host3, Switch3 )
+        self.addLink( Switch3, Switch4 )
+
+        self.addLink( Host4, Switch4 )
+        self.addLink( Switch3, Switch4 )
+
+
+topos = { 'mytopo': ( lambda: MyTopo() ) }
